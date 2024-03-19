bb4ab920dcdd985688d1bd984b5bfa86292a8502
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-03-19 16:28:47 +0530

    Update dev.py

--- DAXXMUSIC/plugins/tools/dev.py
+++ DAXXMUSIC/plugins/tools/dev.py
@@ -7,12 +7,13 @@ from inspect import getfullargspec
 from io import StringIO
 from time import time
 
+
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from DAXXMUSIC import app
-from config import OWNER_ID, EVALOP
-
+from config import OWNER_ID
+###########
 
 async def aexec(code, client, message):
     exec(
@@ -30,13 +31,13 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_edited_message(
     filters.command("eval")
-    & filters.user(EVALOP)
+    & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("eval")
-    & filters.user(EVALOP)
+    & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
@@ -140,13 +141,13 @@ async def forceclose_command(_, CallbackQuery):
 
 @app.on_edited_message(
     filters.command("sh")
-    & filters.user(EVALOP)
+    & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
     filters.command("sh")
-    & filters.user(EVALOP)
+    & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
