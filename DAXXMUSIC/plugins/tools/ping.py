ee93ea3910bdf616b3bfa3c86f26f7444b776981
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-03-19 13:44:13 +0530

    Add files via upload

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -2,21 +2,22 @@ from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import *
 from DAXXMUSIC import app
 from DAXXMUSIC.core.call import DAXX
 from DAXXMUSIC.utils import bot_sys_stats
 from DAXXMUSIC.utils.decorators.language import language
 from DAXXMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS
 
 
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
+    response = await message.reply_video(
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await DAXX.ping()
