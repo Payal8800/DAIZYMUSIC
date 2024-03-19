bbc937cdfd048493a7c54e23e4891f8b3821a09a
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-03-19 13:05:27 +0530

    Add files via upload

--- DAXXMUSIC/plugins/Yumi/write.py
+++ DAXXMUSIC/plugins/Yumi/write.py
@@ -1,6 +1,8 @@
 from pyrogram import filters
+from pyrogram import *
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from config import  BOT_USERNAME
+from datetime import datetime
 from DAXXMUSIC import app as app
 import requests
 
@@ -15,7 +17,7 @@ async def handwrite(_, message: Message):
 
     caption = f"""
 sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
-✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [ᴅᴀɪsʏ](https://t.me/{BOT_USERNAME})
+✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [𝐘ᴜᴍɪᴋᴏᴏ](https://t.me/{BOT_USERNAME})
 🥀 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}
 """
     await m.delete()
@@ -29,3 +31,25 @@ help = """
 
 ❍ /write <ᴛᴇxᴛ> *:* ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
  """
+
+
+#----------
+
+@app.on_message(filters.command("day"))
+def date_to_day_command(client: Client, message: Message):
+    try:
+        # Extract the date from the command message......
+        command_parts = message.text.split(" ", 1)
+        if len(command_parts) == 2:
+            input_date = command_parts[1].strip()
+            date_object = datetime.strptime(input_date, "%Y-%m-%d")
+            day_of_week = date_object.strftime("%A")
+
+            # Reply with the day of the week
+            message.reply_text(f"The day of the week for {input_date} is {day_of_week}.")
+
+        else:
+            message.reply_text("Please provide a valid date in the format `/day 1947-08-15` ")
+
+    except ValueError as e:
+        message.reply_text(f"Error: {str(e)}")
